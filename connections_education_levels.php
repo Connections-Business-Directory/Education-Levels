eb5e5e82a51a2043c408779417164d1b4b49ee4b
Steven A. Zahm <shazahm1@hotmail.com>
2014-05-21 21:31:20 -0400

    Bump version to 1.0.1.

--- connections_education_levels.php
+++ connections_education_levels.php
@@ -13,7 +13,7 @@
  * Plugin Name:       Connections Education Levels
  * Plugin URI:        http://connections-pro.com
  * Description:       An extension for the Connections plugin which adds a metabox for education levels.
- * Version:           1.0
+ * Version:           1.0.1
  * Author:            Steven A. Zahm
  * Author URI:        http://connections-pro.com
  * License:           GPL-2.0+
@@ -72,7 +72,7 @@ if ( ! class_exists('Connections_Education_Levels') ) {
 		 */
 		private static function defineConstants() {
 
-			define( 'CNEL_CURRENT_VERSION', '1.0' );
+			define( 'CNEL_CURRENT_VERSION', '1.0.1' );
 			define( 'CNEL_DIR_NAME', plugin_basename( dirname( __FILE__ ) ) );
 			define( 'CNEL_BASE_NAME', plugin_basename( __FILE__ ) );
 			define( 'CNEL_PATH', plugin_dir_path( __FILE__ ) );

581ca9626474d686db64314b944cf9b91782a0db
Steven A. Zahm <shazahm1@hotmail.com>
2019-07-18 09:37:50 -0400

    Remove use of deprecated function `create_function()` when initiating the plugin.

--- connections_education_levels.php
+++ connections_education_levels.php
@@ -292,10 +292,9 @@ if ( ! class_exists('Connections_Education_Levels') ) {
 
 				add_action(
 					'admin_notices',
-					 create_function(
-						 '',
-						'echo \'<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Education Level.</p></div>\';'
-						)
+					function() {
+						echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Education Level.</p></div>';
+					}
 				);
 
 				return FALSE;
